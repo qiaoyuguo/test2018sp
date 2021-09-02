32ec800f3ad2c8832f6a4f1755cb5fa0825b39c5
Yuguo Qiao <qiaoqiao2003@yeah.net>
2021-09-02 08:35:15 +0800

    finish assert string issue

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -1,12 +1,6 @@
 import org.junit.Test;
 
 public class TestArrayDequeGold {
-    public void printAllOperations(String[] op, int[] param, int n) {
-        for (int i = 0; i < n; i += 1) {
-
-        }
-    }
-
     @Test
     public void testArrayDeque() {
         StudentArrayDeque<Integer> sad = new StudentArrayDeque<>();
@@ -27,16 +21,12 @@ public class TestArrayDequeGold {
             } else if (numberBetweenZeroAndOne < 0.75 && !ads.isEmpty() && !sad.isEmpty()) {
                 int x = ads.removeFirst();
                 int y = sad.removeFirst();
-                if (x != y) {
-                    ops += "removeFirst()\n";
-                }
+                ops += "removeFirst()\n";
                 org.junit.Assert.assertEquals(ops, x, y);
             } else if (!ads.isEmpty() && !sad.isEmpty()) {
                 int x = ads.removeLast();
                 int y = sad.removeLast();
-                if (x != y) {
-                    ops += "removeLast()\n ";
-                }
+                ops += "removeLast()\n";
                 org.junit.Assert.assertEquals(ops, x, y);
             }
         }
